be737d80b4c7c6fc9e487950bfe9fd52e1edcb45
Your Name <you@example.com>
2023-10-12 09:07:45 +0700

    change

--- bot.py
+++ bot.py
@@ -20,26 +20,6 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     username = update.effective_user.username
     chat_id = update.effective_chat.id
 
-    if "/sodu" in update.message.text:
-
-        with open('data.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-
-        message_id = await context.bot.send_message(chat_id, text='<b>Loading 0%</b>', parse_mode=constants.ParseMode.HTML)
-
-        text = "<b>DANH SÁCH SỐ DƯ\n</b>"
-
-        for index, item in enumerate(data):
-            text += f"{index+1}. {item['name']}: {get_balance(item['wallet'])} USDT\n"
-            
-            if index % 5 == 0:
-                percent = f"<b>Loading {round((index+1)/len(data)*100)}%</b>"
-                await context.bot.edit_message_text(chat_id=chat_id, message_id=message_id.message_id, text=percent, parse_mode=constants.ParseMode.HTML)
-
-        await context.bot.delete_message(message_id=message_id.message_id, chat_id=chat_id)
-        await context.bot.send_message(chat_id, text, parse_mode=constants.ParseMode.HTML)
-
-
 async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
 
     with open('data.json', 'r', encoding='utf-8') as f:
