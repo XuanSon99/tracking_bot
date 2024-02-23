7313e7a083d0a5ef150179838fdcc6eb1046d2f8
Your Name <you@example.com>
2024-02-23 14:59:12 +0700

    change

--- bot.py
+++ bot.py
@@ -14,51 +14,78 @@ domain = "https://api.chootc.com"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Tham giá @chootcvn để mua, bán USDT số lượng lớn.", parse_mode=constants.ParseMode.HTML)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Tham giá @chootcvn để mua, bán USDT số lượng lớn.",
+        parse_mode=constants.ParseMode.HTML,
+    )
+
 
 async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.effective_user.username
     chat_id = update.effective_chat.id
-    print(chat_id)
+
+    if "/update" in update.message.text:
+        res = requests.get("https://api.chootc.com/api/tracking")
+        wallets = res.json()
+        data = []
+        for item in wallets:
+            data.append(
+                {
+                    "name": item["name"],
+                    "wallet": item["address"],
+                    "block_timestamp": 1706281575000,
+                }
+            )
+
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2)
+
 
 async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
 
-    with open('data.json', 'r', encoding='utf-8') as f:
+    with open("data.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
     for index, item in enumerate(data):
         res = requests.get(
-            f"https://api.trongrid.io/v1/accounts/{item['wallet']}/transactions/trc20?limit=1&contract_address=TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t")
-        tx = res.json()['data'][0]
-        if item['block_timestamp'] != tx['block_timestamp']:
-            data[index]['block_timestamp'] = tx['block_timestamp']
-            value = round(float(tx['value'])*pow(10, -6))
+            f"https://api.trongrid.io/v1/accounts/{item['wallet']}/transactions/trc20?limit=1&contract_address=TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
+        )
+        tx = res.json()["data"][0]
+        if item["block_timestamp"] != tx["block_timestamp"]:
+            data[index]["block_timestamp"] = tx["block_timestamp"]
+            value = round(float(tx["value"]) * pow(10, -6))
 
             if value >= 50000:
 
-                time = datetime.fromtimestamp(tx['block_timestamp']/1000)
+                time = datetime.fromtimestamp(tx["block_timestamp"] / 1000)
 
-                if tx['from'] == item['wallet']:
+                if tx["from"] == item["wallet"]:
 
-                    account = [p for p in data if p['wallet'] == tx['to']]
-                    acc = tx['to'][-5:]
+                    account = [p for p in data if p["wallet"] == tx["to"]]
+                    acc = tx["to"][-5:]
                     if account:
-                        acc = account[0]['name']
+                        acc = account[0]["name"]
 
                     text = f"🔴 <b>{item['name']}</b> vừa gửi <b>{value}</b> tới ví <b>{acc}</b>\n<a href='https://tronscan.org/#/transaction/{tx['transaction_id']}'>Chi tiết giao dịch</a>"
 
-                if tx['to'] == item['wallet']:
+                if tx["to"] == item["wallet"]:
 
-                    account = [p for p in data if p['wallet'] == tx['from']]
-                    acc = tx['from'][-5:]
+                    account = [p for p in data if p["wallet"] == tx["from"]]
+                    acc = tx["from"][-5:]
                     if account:
-                        acc = account[0]['name']
+                        acc = account[0]["name"]
 
                     text = f"<b>🟢 {item['name']}</b> vừa nhận <b>{value}</b> từ ví <b>{acc}</b>\n<a href='https://tronscan.org/#/transaction/{tx['transaction_id']}'>Chi tiết giao dịch</a>"
 
-                await context.bot.send_message(chat_id=-1001986367510, text=text, parse_mode=constants.ParseMode.HTML, disable_web_page_preview=True)
+                await context.bot.send_message(
+                    chat_id=-1001986367510,
+                    text=text,
+                    parse_mode=constants.ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
 
-    with open('data.json', 'w', encoding='utf-8') as f:
+    with open("data.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
 
 
@@ -70,17 +97,25 @@ def get_balance(address):
     res = requests.get(url, params=payload)
     trc20token_balances = json.loads(res.text)["trc20token_balances"]
     token_balance = next(
-        (item for item in trc20token_balances if item["tokenId"] == 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t'), None)
+        (
+            item
+            for item in trc20token_balances
+            if item["tokenId"] == "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
+        ),
+        None,
+    )
     if token_balance == None:
         return 0
     else:
         return f'{round(float(token_balance["balance"])*pow(10, -6)):,}'
 
+
 app = ApplicationBuilder().token(
     "6568702208:AAGfPceJaWQae39zX57FqqotN3Zx8FWKIUA").build()
 
-# app = ApplicationBuilder().token(
-#     "6673254814:AAHt6zx49L2ARt7Yxr46cyrFF2xJeBpY-gs").build()
+# app = (
+#     ApplicationBuilder().token("6680658242:AAESGOAusAVLvTNtMM-qVAifvv_Qn1haoHc").build()
+# )
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.ALL, messageHandler))
